0cb2cc5ef578770022d5e2c44cbaba92b7a0114f
Gyles Batara <gyles.batara@live.com>
2018-01-08 10:01:29 -0800

    Add coding for Models

--- GummiBearWorld/Models/GummiBearWorldContext.cs
+++ GummiBearWorld/Models/GummiBearWorldContext.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GummiBearKingdom.Models
+{
+	public class GummiBearWoldContext : DbContext
+	{
+		public virtual DbSet<Category> Categories { get; set; }
+		public virtual DbSet<Item> Items { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		=> optionsBuilder
+			.UseMySql(@"Server=localhost;Port=8889;database=todolist;uid=root;pwd=root;");
+	}
+}
--- GummiBearWorld/Models/Product.cs
+++ GummiBearWorld/Models/Product.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList.Models
+{
+	[Table("Product")]
+	public class Item
+	{
+		[Key]
+		public int id { get; set; }
+		public string Description { get; set; }
+		public string CategoryId { get; set; }
+        public int Cost { get; set; }
+		public virtual Category Category { get; set; }
+	}
+}
